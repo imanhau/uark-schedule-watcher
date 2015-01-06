6e0657ed03726a463add0cb298683af91b0d6f39
Bekt <bekt17@gmail.com>
2015-01-05 21:18:29 -0600

    GAE setup.

---
+++ handlers.py
@@ -0,0 +1,10 @@
+import webapp2
+
+app = webapp2.WSGIApplication([
+
+    webapp2.Route(r'/cron/check', name='cron_check', methods=['GET'],
+                  handler='cron.CronHandler:check'),
+    webapp2.Route(r'/cron/process', name='cron_process', methods=['POST'],
+                  handler='cron.CronHandler:process'),
+
+])
